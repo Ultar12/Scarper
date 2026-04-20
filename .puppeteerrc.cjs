4559072d2c744f9261080fe1e7bf9a308bb37695
Ezenwugo Emmanuel Ifechukwu <134739434+Ultar12@users.noreply.github.com>
2026-04-20 17:33:19 -0400

    Update .puppeteerrc.cjs

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,12 +1,18 @@
+const { join } = require('path');
+
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
+  // Save the browser directly into the Heroku app folder so it isn't lost
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+  
   // Completely disable Chrome download
   chrome: {
     skipDownload: true,
   },
-  // ONLY download Firefox
+  
+  // ONLY download Firefox Stable
   firefox: {
     skipDownload: false,
   },
